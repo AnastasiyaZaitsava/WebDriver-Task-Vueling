59047b7267eb9cef828c6c165584d756f91d7804
AnastasiyaMashkevich <nastya.mashkevich@gmail.com>
2017-03-03 18:09:42 +0300

    update

--- testWD/src/test/java/com/epam/pages/VuelingScheduleSelectPage.java
+++ testWD/src/test/java/com/epam/pages/VuelingScheduleSelectPage.java
@@ -25,7 +25,6 @@ public class VuelingScheduleSelectPage extends VuelingAbstractPage {
     @FindBy(id = "ControlGroupScheduleSelectView_AvailabilityInputScheduleSelectView_RadioButtonMkt2Fare1Label")
     private WebElement basicReturnButton;
 
-<<<<<<< HEAD
     @FindBy(id = "ControlGroupScheduleSelectView_AvailabilityInputScheduleSelectView_RadioButtonMkt2Fare2Label")
     private WebElement optimaReturnButton;
 
@@ -42,48 +41,8 @@ public class VuelingScheduleSelectPage extends VuelingAbstractPage {
     }
 
     public void openPage() {
-=======
-     @FindBy(id = "ControlGroupScheduleSelectView_AvailabilityInputScheduleSelectView_RadioButtonMkt1Fare1Label")
-    private WebElement basicOutboundButton;
-    
-    @FindBy(id = "ControlGroupScheduleSelectView_AvailabilityInputScheduleSelectView_RadioButtonMkt1Fare2Label")
-    private WebElement optimaOutboundButton;  
-    
-    @FindBy(id = "ControlGroupScheduleSelectView_AvailabilityInputScheduleSelectView_RadioButtonMkt1Fare3Label")
-    private WebElement excellenceOutboundButton;
-    
-    @FindBy(id = "ControlGroupScheduleSelectView_AvailabilityInputScheduleSelectView_RadioButtonMkt2Fare1Label")
-    private WebElement basicReturnButton;
-    
-    @FindBy(id = "ControlGroupScheduleSelectView_AvailabilityInputScheduleSelectView_RadioButtonMkt2Fare2Label")
-    private WebElement optimaReturnButton;  
-    
-    @FindBy(id = "ControlGroupScheduleSelectView_AvailabilityInputScheduleSelectView_RadioButtonMkt2Fare3Label")
-    private WebElement excellenceReturnButton;
-    
-    
-
-   
-    public VuelingScheduleSelectPage (WebDriver driver)
-    {
-        super(driver);
-        PageFactory.initElements(this.driver, this);
-    }
-    
-    public void openPage()
-    {
->>>>>>> origin/master
         driver.navigate().to(BASE_URL);
     }
-    
-    public void checkTravelInfo(){
-    	basicOutboundButton.click();
-    	basicReturnButton.click();
-    	WebElement element = driver.findElements(By.xpath("//div[@class='travelInfo_list']"));
-    	//if(element.) я не знаю, какая там функция для того чтобы проверить наличие на странице
-    }
-
-    
 
     public boolean chooseFlight() {
         basicOutboundButton.click();
