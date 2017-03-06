0f1e1ac76330eec884fbff6e204403ee84559042
AnastasiyaMashkevich <nastya.mashkevich@gmail.com>
2017-03-06 13:36:25 +0300

    Update Steps.java

--- testWD/src/test/java/com/epam/steps/Steps.java
+++ testWD/src/test/java/com/epam/steps/Steps.java
@@ -1,6 +1,5 @@
 package com.epam.steps;
 
-import com.epam.pages.VuelingChooseSeatAndLuggagePage;
 import com.epam.util.User;
 import com.epam.pages.VuelingContactPassengerPage;
 import com.epam.pages.VuelingMainPage;
@@ -12,21 +11,25 @@ import org.openqa.selenium.WebDriver;
 public class Steps {
     private WebDriver driver;
 
-    public void initBrowser() {
+    public void initBrowser()
+    {
         driver = DriverSingleton.getDriver();
     }
 
-    public void closeDriver() {
+    public void closeDriver()
+    {
         driver.close();
     }
 
-    public void loginToVueling(String user, String psw) {
+    public void loginToVueling (String user, String psw)
+    {
         VuelingMainPage vuelingMainPage = new VuelingMainPage(driver);
         vuelingMainPage.openPage();
-        vuelingMainPage.login(user, psw);
+        vuelingMainPage.login(user,psw);
     }
 
-    public boolean isLoginToVueling() {
+    public boolean isLoginToVueling ()
+    {
         VuelingMainPage vuelingMainPage = new VuelingMainPage(driver);
         return vuelingMainPage.checkIsLogin().contains("Hi");
     }
@@ -41,54 +44,35 @@ public class Steps {
         vuelingContactPassengerPage.enterAndSubmitPassengerContact(user);
     }
 
-    public void fillSeatsAndLuggageInformation(String from, String to, User user) {
-        VuelingContactPassengerPage vuelingContactPassengerPage = new VuelingContactPassengerPage(driver);
-        VuelingScheduleSelectPage vuelingScheduleSelectPage = new VuelingScheduleSelectPage(driver);
-        VuelingMainPage vuelingMainPage = new VuelingMainPage(driver);
-        VuelingChooseSeatAndLuggagePage vuelingChooseSeatAndLuggagePage = new VuelingChooseSeatAndLuggagePage(driver);
-        vuelingMainPage.openPage();
-        vuelingMainPage.startSearch(from, to);
-        vuelingScheduleSelectPage.chooseFlight();
-        vuelingContactPassengerPage.enterAndSubmitPassengerContact(user);
-        vuelingChooseSeatAndLuggagePage.chooseSeatAndAddLuggage();
-    }
-
-    public boolean isChangeContactInfo(String country) {
+    public boolean isChangeContactInfo (String country) {
         VuelingMainPage vuelingMainPage = new VuelingMainPage(driver);
         vuelingMainPage.openPage();
-        //vuelingMainPage.toChangeContactInfo(country);
         if (vuelingMainPage.toChangeContactInfo(country)) {
             return true;
         }
         return false;
     }
 
-    public boolean checkFlight(String from, String to) {
+    public boolean checkFlight (String from, String to) {
         VuelingMainPage vuelingMainPage = new VuelingMainPage(driver);
         VuelingScheduleSelectPage vuelingScheduleSelectPage = new VuelingScheduleSelectPage(driver);
         vuelingMainPage.openPage();
-        vuelingMainPage.startSearch(from, to);
+        vuelingMainPage.startSearch( from,  to);
 
-        if (vuelingScheduleSelectPage.chooseFlight()) {
+        if(vuelingScheduleSelectPage.chooseFlight()){
             return true;
-        } else {
+        }else {
             return false;
         }
-
     }
 
-    public boolean isFillInfoCorrect(String from, String to, User user) {
+    public boolean isFillInfoCorrect (String from, String to, User user) {
         VuelingContactPassengerPage vuelingContactPassengerPage = new VuelingContactPassengerPage(driver);
         vuelingContactPassengerPage.enterAndSubmitPassengerContact(user);
-        if (vuelingContactPassengerPage.clickSubmit()) {
+        if(vuelingContactPassengerPage.clickSubmit()) {
             return true;
         }
         return false;
-    }
-
-    public boolean isFillInfoSeatAndLuggageCorrect() {
-        VuelingChooseSeatAndLuggagePage vuelingChooseSeatAndLuggagePage = new VuelingChooseSeatAndLuggagePage(driver);
-        return vuelingChooseSeatAndLuggagePage.isServiceBlockDisplayed();
     }
 
 }
