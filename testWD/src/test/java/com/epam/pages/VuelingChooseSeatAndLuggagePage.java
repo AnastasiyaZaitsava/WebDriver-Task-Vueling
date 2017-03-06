a3f36616c8b6dbdeec1fcbd58f7bdb55a8a91664
AnastasiyaMashkevich <nastya.mashkevich@gmail.com>
2017-03-06 13:34:46 +0300

    Update VuelingChooseSeatAndLuggagePage.java

--- testWD/src/test/java/com/epam/pages/VuelingChooseSeatAndLuggagePage.java
+++ testWD/src/test/java/com/epam/pages/VuelingChooseSeatAndLuggagePage.java
@@ -2,51 +2,24 @@ package com.epam.pages;
 
 import org.openqa.selenium.WebDriver;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.Select;
 
 
-public class VuelingChooseSeatAndLuggagePage extends VuelingAbstractPage {
-
-    private final String BASE_URL = "http://www.vueling.com/en";
-
-    @FindBy(id = "sec1")
-    private WebElement chooseSeat;
-
-    @FindBy(id = "seat_0_4A")
-    private WebElement chooseConcreteSeat;
-
-    @FindBy(className = "wrap_btSmall_plus")
-    private WebElement addLuggageButton;
-
-    @FindBy(id = "CONTROLGROUPSERVICES_BaggageViewServicesView_SSRBaggageInputViewServicesView_SSRCode_PASSENGERCOUNT_0_SSRCOUNT_0")
-    private WebElement dropDownLuggageList;
-
-    @FindBy(className = "travelInfo_list travelInfo_list--services")
-    private WebElement serviceBlock;
+public class VuelingChooseSeatAndLuggagePage extends VuelingAbstractPage{
+        private final String BASE_URL = "http://www.vueling.com/en"; //изменить на нужный
 
 
-    public VuelingChooseSeatAndLuggagePage(WebDriver driver) {
+
+    public VuelingChooseSeatAndLuggagePage (WebDriver driver)
+    {
         super(driver);
         PageFactory.initElements(this.driver, this);
     }
 
-    public void openPage() {
+
+    public void openPage()
+    {
         driver.navigate().to(BASE_URL);
-    }
-
-    public void chooseSeatAndAddLuggage() {
-        Select dropDownListLuggageSelect = new Select(dropDownLuggageList);
-        chooseSeat.click();
-        chooseConcreteSeat.click();
-        addLuggageButton.click();
-        dropDownListLuggageSelect.selectByVisibleText("1 Suitcase");
-    }
-
-    public boolean isServiceBlockDisplayed() {
-        return serviceBlock.isDisplayed();
     }
 
 }
