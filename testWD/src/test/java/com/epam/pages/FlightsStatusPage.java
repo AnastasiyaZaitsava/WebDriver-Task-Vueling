cc93a353552cabd061086a8a2d3de5d831dd0d7e
olgagitskaya <ogitskaya@gmail.com>
2017-03-09 14:29:49 +0300

    Update FlightsStatusPage.java

--- testWD/src/test/java/com/epam/pages/FlightsStatusPage.java
+++ testWD/src/test/java/com/epam/pages/FlightsStatusPage.java
@@ -1,12 +1,15 @@
 package com.epam.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 
 public class FlightsStatusPage extends AbstractPage {
