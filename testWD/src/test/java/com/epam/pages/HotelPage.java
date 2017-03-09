eb6648c5436dcfa337b517fc0f9b1fbd54d35316
AnastasiyaZaitsava <anastasiya_zaitsava@epam.com>
2017-03-09 15:36:26 +0300

    Add files via upload

---
+++ testWD/src/test/java/com/epam/pages/HotelPage.java
@@ -0,0 +1,55 @@
+package com.epam.pages;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HotelPage extends AbstractPage{
+	 private final String BASE_URL = "http://hotel.vueling.com/?label=vlng-tab-home&lang=en";
+
+	public HotelPage(WebDriver driver) {
+		super(driver);
+		PageFactory.initElements(this.driver, this);
+	}
+
+	@Override
+	public void openPage() {
+		 driver.navigate().to(BASE_URL);
+		
+	}
+	
+	@FindBy(id = "ss")
+	    private WebElement inputDest;
+	 
+	
+	@FindBy(xpath = "//div[@data-placeholder = 'Check-in date']")
+		private WebElement checkinDateBox;
+
+	@FindBy(xpath = "//div[@data-placeholder = 'Check-out date']")
+	private WebElement checkoutDateBox;
+	
+	@FindBy(xpath = "//td[@class='c2-day ']")
+	private WebElement checkinDate;
+	
+	@FindBy(xpath = "*//button[@type='submit']")
+	private WebElement submitButton;
+	
+	public void fillInfo(){
+		 inputDest.sendKeys("Minsk");
+		 checkinDateBox.click();
+		 checkinDate.click();
+		 checkoutDateBox.click();
+	}
+	
+	  public boolean clickSubmit () {
+          if(submitButton.isDisplayed()) {
+              JavascriptExecutor jse = (JavascriptExecutor) driver;
+              jse.executeScript("arguments[0].click();", submitButton);
+              return true;
+          } else {
+              return false;
+          }
+      }
+}
